7078c11d3a0ac27fb97980b1d2350e477f4d49e5
Idan-Alashvili <idan2004001@gmail.com>
2024-09-15 14:20:30 +0300

    Update Car.java

--- cs projects/Java/CarDealership(big)/Car.java
+++ cs projects/Java/CarDealership(big)/Car.java
@@ -1,6 +1,5 @@
 package CarDealership;
-// Assignment: 5
-// Author: Idan Alashvili, ID: 326117629
+// Author: Idan Alashvili
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
